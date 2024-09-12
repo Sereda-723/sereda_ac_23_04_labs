fd0edded1e8fd96810221b03bcab3d83a8207df2
Vladimir_Sereda_laptop <vovasereda541@gmail.com>
2024-09-12 13:12:43 +0300

    almost

--- sereda_ac_23_04_lab_1/sereda_ac_23_04_lab_1.cpp
+++ sereda_ac_23_04_lab_1/sereda_ac_23_04_lab_1.cpp
@@ -7,11 +7,15 @@ using namespace std;
 
 int main() {
 
+    setlocale(LC_ALL, "Russian");
+
     cout << "Helllo world!!!" << endl;
 
     char x;
     cin >> x;
     
     cout << "Привет‚ "s << x << endl;
+    cout << "йцукенгшщзхъэждлорпавыфячсмитьбю "s << x << endl;
     return 0;
+
 }
