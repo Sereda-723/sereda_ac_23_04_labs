057e286f3567ead7c748a44f918b690907b2163b
Vladimir_Sereda_laptop <vovasereda541@gmail.com>
2024-09-12 12:18:55 +0300

    lab-1

---
+++ sereda_ac_23_04_lab_1/sereda_ac_23_04_lab_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+
+
+using namespace std;
+
+int main() {
+
+    cout << "Helllo world!!!" << endl;
+
+    char x;
+    cin >> x;
+    
+    cout << "Привет‚ "s << x << endl;
+    return 0;
+}
