c21bed96ce615136ac5c9c670704d222cce82599
Vladimir_Sereda <vovasereda541@gmail.com>
2024-10-02 18:43:24 +0300

    void EditStatusStation

--- sereda_ac_23_04_lab_1/sereda_ac_23_04_lab_1.cpp
+++ sereda_ac_23_04_lab_1/sereda_ac_23_04_lab_1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <limits>
 
 
 using namespace std;
@@ -30,18 +31,22 @@ void AddPipe(Pipe& p) {
     getline(cin, p.name);
 
     cout << "Enter the length of the pipe (positive value): ";
-
     while(!(cin >> p.length) || p.length <=0) {
-        cout << "Invalid value. Enter a positive number";
+        cout << "Invalid value. Enter a positive number: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
-    cout << "Enter the diameter of the pipe: ";
 
+    cout << "Enter the diameter of the pipe: ";
     while(!(cin >> p.diameter) || p.diameter <=0) {
-        cout << "Invalid value. Enter a positive number";
+        cout << "Invalid value. Enter a positive number: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
     p.inrepair = false;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 }
 
 // Вывод информации о трубек
@@ -69,8 +74,11 @@ void EditStatusPipe(Pipe& p) {
     int status;
     while (!(cin >> status) || (status != 0 && status != 1)) {
         cout << "Invalid value. Enter 1 or 0 ( 1 - In repairing, 0 - Running): ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
     p.inrepair = status;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
 }
 
@@ -83,18 +91,96 @@ void AddStation(Station& s) {
     cout << "Enter the number of workshops (Positive value): ";
 
     while(!(cin >> s.workshops) || s.workshops <=0) {
-        cout << "Invalid value. Enter a positive number";
+        cout << "Invalid value. Enter a positive number: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
     cout << "Enter the number of Active workshops: ";
 
     while(!(cin >> s.activeworkshops) || s.activeworkshops <=0 || s.activeworkshops > s.workshops) {
         cout << "Invalid value. Enter a number from 0 to " << s.workshops << ": ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
     cout << "Enter the efficiency of workshop: ";
 
     while(!(cin >> s.station_efficiency) || s.station_efficiency <= 0) {
-        cout << "Invalid value. Enter a positive number";
+        cout << "Invalid value. Enter a positive number: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
+
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
+
+
+// Вывод информации о станции
+void ViewStation(Station s) {
+    cout << "Information about the station:";
+    cout << "Name: " << s.name << endl;
+    cout << "Number of workshops: " << s.workshops << endl;
+    cout << "Number of Active workshops: " << s.activeworkshops << endl;
+    cout << "Efficiency: " << s.station_efficiency << endl;
+
+}
+
+
+// Статус станции
+
+void EditStatusStation(Station& s) {
+    if (s.name.empty()) {
+        cout << "Pipe was not found.";
+        return;
+    }
+
+    cout << "The current number of Active workshops:" << s.activeworkshops << endl;
+    cout << "Select an action: " << endl;
+    cout << "1. Start the workshop" << endl;
+    cout << "2. Stop the workshop" << endl;
+    cout << "Enter the number (1 or 2): ";
+
+    int choice;
+    while (!(cin >> choice) || (choice != 1 && choice != 2)) {
+        cout << "Incorrect choice. Enter '1' to start or '2' to stop the workshop: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    }
+
+    if (choice == 1){
+        if (s.activeworkshops < s.workshops) {
+            s.activeworkshops++;
+            cout << "The workshop is running. The current number of active workshops: " << s.activeworkshops << "/" << s.workshops << endl;
+        } else {
+            cout << "It is impossible to start a new workshop. All workshops are already working. The current number of active workshops: " << s.activeworkshops << "/" << s.workshops << endl;
+        }
+    } else if (choice == 2){
+        if (s.activeworkshops > 0) {
+            s.activeworkshops--;
+            cout << "The workshop is stopped. The current number of active workshops: " << s.activeworkshops << "/" << s.workshops << endl;
+        } else {
+            cout << "All workshops have already been stopped. The current number of active workshops: " << s.activeworkshops << "/" << s.workshops << endl;
+        }
+    }
+
+     cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+
+
+
+
+
+
+    int status;
+    while (!(cin >> status) || (status != 0 && status != 1)) {
+        cout << "Invalid value. Enter 1 or 0 ( 1 - In repairing, 0 - Running): ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    p.inrepair = status;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
 }
